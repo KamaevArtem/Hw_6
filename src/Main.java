65419defc3ba2c99617516951484c9d140238f9e
krito <kritomania1991@gmail.com>
2024-10-30 22:17:35 +0300

    Hw_6

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         //Задача 1
-        for (int i = 0; i <= 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
         //Задача 2
@@ -9,7 +9,7 @@ public class Main {
             System.out.println(i);
         }
         //Задача 3
-        for (int i = 0; i < 17; i = i+2){
+        for (int i = 0; i <= 17; i = i+2){
             System.out.println(i);
         }
         //Задача 4
@@ -18,7 +18,7 @@ public class Main {
         }
         //Задача 5
         for (int i = 1904; i <= 2096; i = i + 4){
-            System.out.println(i);
+            System.out.println(i + " год является високосным");
         }
         //Задача 6
         for (int i = 7; i <=98; i = i + 7){
@@ -48,7 +48,7 @@ public class Main {
         int  numberAfterOperations;
         for (int i = 1; i<=10; i++){
             numberAfterOperations = number * i;
-            System.out.println(numberAfterOperations);
+            System.out.println(number + " * " + i + " = " + numberAfterOperations);
         }
 
     }
